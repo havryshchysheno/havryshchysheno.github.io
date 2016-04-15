af48c1764ac8aabfd49ad5e53872d7bd6b54b084
Aleksey Havryshchyshen <aleksei906@bigmir.net>
2016-04-15 12:56:31 +0300

    v 1.0

---
+++ JS_03-04/js/script.js
@@ -0,0 +1,81 @@
+var startBtn = document.createElement('button');
+var parent = document.body;
+parent.className = 'text-center'
+startBtn.innerHTML = 'Press button'
+startBtn.className = 'btn btn-danger';
+startBtn.id = 'start'
+parent.appendChild(startBtn);
+
+document.getElementById('start').addEventListener('click', createDoc);
+
+var dynamicArr = {
+	documentHead: 'Тест по программированию',
+	listHead: 'Вопрос №',
+	listVar: 'Вариант ответа №',
+	buttonText: 'Проверить мои результаты',
+	container: function () {
+		var container = document.createElement('div');
+		container.className = 'container text-center';
+		document.body.appendChild(container);
+	},
+	headPush : function () {
+		var head = document.createElement('h1');
+		var parent = document.querySelector('.container')
+		head.innerHTML = this.documentHead;
+		parent.appendChild(head);
+	},
+	listHeader: function (x) {
+		var listHead = document.createElement('h2');
+		var parent = document.querySelector('.container')
+		listHead.className = 'text-left'
+		listHead.innerHTML = this.listHead + x;
+		parent.appendChild(listHead);
+	},
+	emptyList: function (x) {
+		var list = document.createElement('ul');
+		var parent = document.querySelector('.container');
+		list.className = 'list-unstyled text-left';
+		list.id = 'list-'+x
+		parent.appendChild(list);
+	},
+	listItem: function(x) {
+		var listVar = document.createElement('li');
+		var check = document.createElement('input');
+		var label = document.createElement('label');
+		var parent = document.getElementById('list-'+i);
+		label.innerHTML = this.listVar + x;
+		label.setAttribute( 'for' , 'check-' + i + '-' + x );
+		check.type = "checkbox";
+		check.id = 'check-' + i + '-' + x
+		parent.appendChild(listVar);
+		listVar.appendChild(check);
+		listVar.appendChild(label);
+	},
+	button: function () {
+		var button = document.createElement('button');
+		button.innerHTML = this.buttonText;
+		button.className = 'btn btn-primary'
+		var parent = document.querySelector('.container');
+		parent.appendChild(button);
+	}
+};
+
+function createDoc () {
+var hideBtn = document.getElementById('start');
+hideBtn.className = 'hidden';
+dynamicArr.container();
+dynamicArr.headPush() ;
+
+for (i = 1; i < 4; i++) {
+
+	dynamicArr.listHeader(i);
+	dynamicArr.emptyList(i);
+
+	for (j = 1; j < 4; j++) {
+		var listName = document.querySelectorAll('ul');
+		dynamicArr.listItem(j);
+		console.log(listName[j]);
+	}
+};
+dynamicArr.button();
+}
