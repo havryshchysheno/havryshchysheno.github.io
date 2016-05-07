70db8144c035edebc8702dabc133cade501d01d3
Aleksey Havryshchyshen <aleksei906@bigmir.net>
2016-05-07 12:41:10 +0300

    v 1.0

--- JS_07-08/js/script.js
+++ JS_07-08/js/script.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
-    $(".tab-link").click(function(event) {
-        event.preventDefault();
-        $(this).parent().addClass("active");
-        $(this).parent().siblings().removeClass("active");
-        var tab = $(this).attr("href");
-        $(".tab").not(tab).hide();
-        $(tab).fadeIn();
-    });
+	
+	$('.tab-link').click(function () {
+		$(this).parent().addClass('active');
+		$(this).parent().siblings().removeClass('active');
+		$('.tab').not($(this).attr('href')).hide();
+		$($(this).attr('href')).fadeIn(1000);
+		return false;
+	})
+
+	$('.input-data').hover(
+		function () {
+		log = $(this).next();
+		console.log(log);
+		$(this).next().fadeIn();
+		},
+		function () {
+			$(this).next().fadeOut();
+		} 
+	)
+
+	$('.help-btn').click (function () {
+		$('.form-tooltip').fadeIn();
+		setTimeout(function () {
+			$('.form-tooltip').fadeOut();
+		}, 5000)
+		return false;
+	})
 });
