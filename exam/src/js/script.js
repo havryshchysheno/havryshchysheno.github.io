2f29b70569ecab82b4a658021cd97184e06f9c17
Aleksey Havryshchyshen <aleksei906@bigmir.net>
2016-07-11 11:57:30 +0300

    v 0.2

--- exam/src/js/script.js
+++ exam/src/js/script.js
@@ -1,55 +1,10 @@
 $(function () {
 	
-	 $('.holidays_grid').masonry({
-		itemSelector: '.holidays-item'
-	 });
+	// $('.holidays_grid').masonry({
+	// 	itemSelector: '.holidays-item'
+	// });
 
-
-
-$('.holidays_button').on('click', pic);
-
-// 	function pictures (e) {
-// 		var input = $('.holidays_input').val();
-// 		$.ajax({
-// 			dataType: 'json',
-// 	  		url: "http://api.pixplorer.co.uk/image?word=" + $('.holidays_input').val() + "&amount=7&size=sm"
-// 		})
-// 	  	.done(function( data ) {
-// 	  		var images = data.images;
-// 	  		var template = tmpl($('#image_tmpl').html(), {data: images});
-
-// 	  		$('.holidays_grid').html(template);
-// 	      		console.log( "Sample of data:", images);
-// 	  	});
-// 	  	return false;
-// }
-
-function pic () {
-var input = $('.holidays_input').val()
-
-$.ajax({
-	url : "http://api.flickr.com/services/feeds/photos_public.gne",
-	dataType: 'jsonp',
-	data: { "tags": input, "format": "json" }
-});
-
-	jsonFlickrFeed = function(d){
-		var json = d;
-		var data = [];
-		var title = [];
-
-		for (var i = 0; i < 7; i++) {
-			data.push(json.items[i].media.m.replace('_m', '_z'));
-			title.push(json.title.replace('Recent Uploads t', 'T'));
-			};
-
-		var template = tmpl($('#image_tmpl').html(), {data: data, title});
-
-		$('.holidays_grid').html(template);
-	}
-
-	return false
-}
+	//getImage('.holidays_button');
 
 	$('#slider-1').flexslider({
 		animation: 'fade',
@@ -81,6 +36,34 @@ $.ajax({
 		nextText: ''
 	});
 
-pic();
 
+// function getImage (item) {
+// 	$(item).on('click', getImage);
+// 	var input = $('.holidays_input').val();
+
+// $.ajax({
+// 	url : "http://api.flickr.com/services/feeds/photos_public.gne",
+// 	dataType: 'jsonp',
+// 	data: { "tags": input, "format": "json" }
+// });
+
+// 	jsonFlickrFeed = function(d){
+// 		var json = d;
+// 		var data = [];
+// 		var title = [];
+
+// 		for (var i = 0; i < 7; i++) {
+// 			data.push(json.items[i].media.m.replace('_m', '_z'));
+// 			title.push(json.title.replace('Recent Uploads t', 'T'));
+// 			};
+
+// 		var template = tmpl($('#image_tmpl').html(), {data: {data, title}});
+
+// 		$('.holidays_grid').html(template);
+// 	};
+
+// 	return false;
+// }
+
+	
 })
