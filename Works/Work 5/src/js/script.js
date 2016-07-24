c42cbae05bb69b05d0f306342ca0a0331b7117d6
Aleksey Havryshchyshen <aleksei906@bigmir.net>
2016-07-24 11:29:15 +0300

    v 0.1

---
+++ Works/Work 5/src/js/script.js
@@ -0,0 +1,59 @@
+$(window).on('load', function () {
+	var $preloader = $('.page-preloader'),
+	$spinner   = $preloader.find('.spinner');
+	$spinner.fadeOut();
+	$preloader.delay(350).fadeOut('slow');
+});
+
+$(function () {
+
+		// Mobile menu
+$( '#mobile-menu' ).multilevelpushmenu({
+	fullCollapse: true,
+	collapsed: true
+});
+
+$( '#show-menu' ).click(function(){
+	$( '#mobile-menu' ).multilevelpushmenu( 'expand' );
+});
+
+$('.content').click(function () {
+	$('#mobile-menu').multilevelpushmenu( 'collapse' );
+}) 
+
+
+	//Range slider
+$("#range").ionRangeSlider({
+	hide_min_max: false,
+	keyboard: true,
+	min: 20,
+	max: 200,
+	from: 60,
+	to: 160,
+	type: 'double',
+	step: 10,
+	prefix: "$",
+	grid: false
+});
+
+
+	//Custom checkbox
+$('.filter_checkbox-label').mousedown(
+	function () {
+		changeCheck($(this));
+	});
+
+function changeCheck (el) {
+	input = el.find('input');
+	if (!input.hasClass('active')) {
+		input.parent().css('background-position', '0 0');
+		input.attr('checked','checked');
+		input.addClass('active')
+	} else {
+		input.parent().css('background-position', '-20px 0');
+		input.removeAttr('checked');
+		input.removeClass('active')
+	}
+} 
+	
+})
